44ca624ccbc9fa33186b9ddc1ebfb061c43e7538
Florindo Silote <florindosiloti@gmail.com>
2018-04-10 11:56:04 -0300

    First commit - Criacao do projeto

---
+++ src/main/java/br/com/tree/bolao/BolaoApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.tree.bolao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolaoApiApplication.class, args);
+	}
+}
